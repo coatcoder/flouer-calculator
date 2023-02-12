84b4e90f3dec81cdba3ee3a1b094ece9773475f0
coatcoder <lidskyalexey1337@gmail.com>
2023-02-12 12:12:33 -0800

    Update st.py

--- st.py
+++ st.py
@@ -11,7 +11,7 @@ names = []
 for key,value in data.items():
    names.append(key)
 
-option = st.selectbox(options=names, label='Выберите цветок')
+option = st.selectbox(options=names, label='Выберите цветы : ')
 
 value = data.get(option)
 st.write("Название : ", option)
@@ -19,7 +19,7 @@ amount = st.number_input(min_value=10, step=10, label="Количество цв
 nacenka = st.number_input(min_value=0, max_value=100, step=1, label="Введите наценку : ")
 izn = float(value)*float(float(amount)/10)
 itog = (float(value) + (float(value) / 100) * nacenka)*float(float(amount)/10)
-st.write("Цена за 10 шт. : ", float(value), "Изначальная цена : ", izn, "Цена с наценкой: ", itog)
+st.write("Цена за 10 шт. : ", float(value), "Цена без наценки : ", izn, "Цена с наценкой: ", itog)
 
 
 #data[option]
