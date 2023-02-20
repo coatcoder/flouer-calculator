849d81e84a2041119a34ad641a3f8b8f2ab6e2da
coatcoder <lidskyalexey1337@gmail.com>
2023-02-20 03:35:07 -0800

    v1.2-update

--- floUer.py
+++ floUer.py
@@ -1,28 +1,30 @@
 import requests
 
-data = {}
-page = requests.get("https://opt-fp.ru/catalog/srezannye-cvety")
-htmla = page.text
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
-    from bs4 import BeautifulSoup                      #На заметку : переписать всё под pandas
-
-
-def get_flower():
-    html = htmla
+    from bs4 import BeautifulSoup
+#Добавить выбор сайта, проработать алгоритм для других сайтов.
+def count_pages_opt_fp_ru():
+    pg_nums_opt_fp_ru = []
+    for i in range(1, 278):
+        pg_nums_opt_fp_ru.append(i)
+    return pg_nums_opt_fp_ru
+def get_flower_opt_fp_ru(page_number):
+    data = {}
+    print("[+] Page number : ", page_number)
+    page = requests.get("https://opt-fp.ru/catalog/srezannye-cvety",params=dict(page=page_number))
+    html = page.text
+    print("[+] Page selected : https://opt-fp.ru/catalog/srezannye-cvety?page=", page_number)
     parsed_html = BeautifulSoup(html, features="lxml")
     parsed_name = parsed_html.body.find_all('div', attrs={'class': 'product-box__name'})
     parsed_price = parsed_html.body.find_all('span', attrs={'class': 'price-result'})
+    for i in range(0, len(parsed_price)):
+        a = BeautifulSoup.get_text(parsed_name[i])
+        name = a[1:-1]
+        a = BeautifulSoup.get_text(parsed_price[i])
+        price = a[26:-20]
+        data[name] = price
+    return data
 
-    def get_info():
-        def get_all():  # Ужасный код. Оптимизируй это пожалуйста. ЗАЧЕМ СТОЛЬКО ПРОЦЕДУР!!!
-            for i in range(0, 14):
-                a = BeautifulSoup.get_text(parsed_name[i])
-                name = a[1:-1]
-                a = BeautifulSoup.get_text(parsed_price[i])
-                price = a[26:-20]
-                data[name] = price
-        get_all()
-    get_info()
 
--- st.py
+++ st.py
@@ -1,37 +1,31 @@
 from floUer import *
 import streamlit as st
 
+pg_nums = count_pages_opt_fp_ru()
+st.title('FloUer Калькулятор v1.2')
+page_selected = st.slider('Страница сайта', 1, pg_nums[-1], 1)
 
-st.title('FloUer Калькулятор v1.1a')
-
-
-get_flower()
+# P.S: 19/2/23 23:10 - Помогите.
+# P.S (2): 20/2/23 15:31 - УРААААААА. Выполнение алгоритма теперь занимает больше времени...
 names = []
-
-# Ради всего святого оптимизируй, это занимает слишком много ресурсов и времени, может привести к ошибкам.
-# Еще раз напоминаю себе о переработке алгоритма в более удобную структуру.
-
-for key,value in data.items():
-   names.append(key)
-
+data = get_flower_opt_fp_ru(page_selected)
+for key, value in data.items():
+    names.append(key)
 option = st.selectbox(options=names, label='Выберите цветы : ')
-
 value = data.get(option)
 st.write("Название : ", option)
 if float(value) == 0.0:
-   st.write("**Цена недоступна для данных цветов.**")
+    st.write("**Цена недоступна для данных цветов.**")
 else:
-   amount = st.number_input(min_value=1, step=1, label="Количество цветов (в 10 шт.) : ")
-   nacenka = st.number_input(min_value=0, max_value=100, step=1, label="Введите наценку (в %): ")
-   izn = float(value) * float(float(amount))
-   itog = (float(value) + (float(value) / 100 * nacenka))* float(amount)
-   pribil = itog - izn
-   rentabel = float(pribil / izn * 100)
+    amount = st.number_input(min_value=1, step=1, label="Количество цветов (в 10 шт.) : ")
+    nacenka = st.number_input(min_value=0, max_value=100, step=1, label="Введите наценку (в %): ")
+    izn = float(value) * float(float(amount))
+    itog = (float(value) + (float(value) / 100 * nacenka)) * float(amount)
+    pribil = itog - izn
+    rentabel = float(pribil / izn * 100)
 
-   st.write("Цена за 10 шт. : ", round(float(value),2), " руб.")
-   st.write("Цена без наценки : ", round(izn,2), " руб.")
-   st.write("**Цена с наценкой :**", round(itog,2), "**руб.**")
-   st.write("**Прибыль :**", round(pribil, 2), "**руб.**")
-   st.write("**Рентабельность :**", round(rentabel), "**%**")
+    st.write("Цена за 10 шт. : ", round(float(value), 2), " руб.")
+    st.write("Цена без наценки : ", round(izn, 2), " руб.")
+    st.write("**Цена с наценкой :**", round(itog, 2), "**руб.**")
+    st.write("**Прибыль :**", round(pribil, 2), "**руб.**")
 
-#data[option]
